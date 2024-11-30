dfabab3297574e62ae4dda010d69343ee8830ca2
Ian Titor <ianpatricktan@gmail.com>
2024-11-30 13:05:20 -0500

    webserver serving styles, safer prompts (VERY IMPORTANT)

--- ai-backend/webserver.py
+++ ai-backend/webserver.py
@@ -1,18 +1,51 @@
 from flask import Flask, send_file, request
 from genutils import Generator
 
-gen = Generator()
+STYLE = {
+        "still": {
+            "ckpt": "liberteRedmond_v10.safetensors",
+            "prompt": "best quality, real photo, professional still-life photography, RAW image",
+            "negative": "worst quality, NSFW, porn, suggestive image, text, watermark, signature",
+            "sampler": "euler",
+            "steps": 20,
+            "cfg": 8,
+            },
+        "anime": {
+            "ckpt": "CounterfeitV30_v30.safetensors",
+            "prompt": "best quality, simple, girl, headshot",
+            "negative": "worst quality, NSFW, porn, suggestive image, text, watermark, signature",
+            "sampler": "euler",
+            "steps": 20,
+            "cfg": 8,
+            },
+        "real": {
+            "ckpt": "majicmixRealistic_v7.safetensors",
+            "prompt": "best quality, real photo, professional photography, RAW image, model posing, adult woman",
+            "negative": "worst quality, NSFW, porn, suggestive image, text, watermark, signature",
+            "sampler": "euler",
+            "steps": 20,
+            "cfg": 8,
+            },
+        }
+
+gen = Generator(ckpt_name="majicmixRealistic_v7.safetensors")
 
 app = Flask(__name__)
 
 @app.route("/generate", methods=["GET"])
 def send_image():
     global gen
-    #filename = request.args.get("filename", "default.jpg")
+    style = request.args.get("style", "anime")
     try:
-        gen()
-        file_path = "images/image.png"
-        return send_file(file_path, mimetype="image/png")
+        gen(**STYLE[style])
+        return send_file("images/image.png", mimetype="image/png")
+    except Exception as e:
+        return str(e), 404
+
+@app.route("/lineart", methods=["GET"])
+def send_lineart():
+    try:
+        return send_file("images/lineart.png", mimetype="image/png")
     except Exception as e:
         return str(e), 404
 
