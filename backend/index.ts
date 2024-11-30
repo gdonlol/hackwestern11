d8eae0f5d90128c3057927bae373ba5d08b140b4
gdonlol <gordon.liang.xu@gmail.com>
2024-11-30 06:04:00 -0500

    sadasdsa

--- backend/index.ts
+++ backend/index.ts
@@ -7,9 +7,13 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/api/generate", async (req, res) => {
-    const url = ""
+    const url = "http://129.100.196.65:6969/generate"
+    console.log('sending get req')
     const balls = await axios.get(url, { responseType: "stream" })
+    console.log("done")
+    console.log(balls)
     res.setHeader("Content-Type", "image/png")
+    balls.data.pipe(res)
 });
 
 app.listen(PORT, () => {
