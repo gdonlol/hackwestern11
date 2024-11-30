0394455c4067a01173a1617c347b359e96802261
gdonlol <gordon.liang.xu@gmail.com>
2024-11-30 00:25:19 -0500

    header comp

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <div className="header">
+        <h1 className="logo-title">LOGO</h1>
+    </div>
+  )
+}
+
+export default Header
