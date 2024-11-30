6c3e9f394b0a6e077adf5c4ce15ffc6789cfe473
Basil Liu <basil.liu18@gmail.com>
2024-11-30 17:04:52 -0500

    back to no auth0

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,37 +1,14 @@
 import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const Header = () => {
-  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
-  const [showDropdown, setShowDropdown] = useState(false);
+type Props = {}
 
-  const handleLogoClick = () => setShowDropdown((prev) => !prev);
-
+function Header({}: Props) {
   return (
     <div className="header">
-      <h1>ArtMentor</h1>
-      <div className="auth-controls">
-        {!isAuthenticated ? (
-          <button className="login-logout-btn btn" onClick={() => loginWithRedirect()}>Log In</button>
-        ) : (
-        <div className="authenticated">
-            <img
-              src={user?.picture || "/default-avatar.png"}
-              alt="User"
-              className="logo"
-              onClick={handleLogoClick}
-            />
-            {showDropdown && (
-              <div className="dropdown">
-                <button className="btn">View Profile</button>
-              </div>
-            )}
-            <button className="login-logout-btn btn" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>
-          </div>
-        )}
-      </div>
+        <h1>ArtMentor</h1>
     </div>
-  );
-};
+  )
+}
 
 export default Header;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,19 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from '@auth0/auth0-react';
 
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Auth0Provider
-      domain="dev-y3z8jvples0v2xmc.us.auth0.com"
-      clientId="FKF49MUJgYdPbIhb7JmIYAMj4NkYBL7F"
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </BrowserRouter>,
 )
